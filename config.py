f7433b6627ad3bcd3da5bb53ac8efc76d600e6fb
QuackerFriends <quackersnfts@gmail.com>
2026-06-28 20:52:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
 
+import os
+from web3 import Web3
+
+# Discord
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+# Blockchain
+RPC_URL = os.getenv("RPC_URL")
+
+CONTRACT_ADDRESS = Web3.to_checksum_address(
+    os.getenv("CONTRACT_ADDRESS")
+)
+
+MAX_SUPPLY = int(os.getenv("MAX_SUPPLY", "3000"))
+
+# Discord Channel
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
