6b1cb5a88e05fdd1207b4faf07913c7bd9fb82a1
QuackerFriends <quackersnfts@gmail.com>
2026-06-30 01:15:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def on_ready():
 
     print(f"Found channel: {channel.name}")
 
-    latest =  47976758 
+    latest =  47977670 
 
     logs = w3.eth.get_logs({
         "fromBlock": latest,
